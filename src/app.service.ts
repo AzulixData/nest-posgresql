0062cd332b5ebe3e12e3ef4f2b2d3f60fb6d16d2
ElAzulix <azulrami03@gmail.com>
2023-03-30 01:22:53 -0600

    add basic controller

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  public getHello(): string {
-    console.log('hola servicio');
-    return 'Hello World!';
-  }
-}
